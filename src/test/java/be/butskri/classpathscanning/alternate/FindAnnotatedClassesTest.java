63ca59af61cbee7f2d882b9ad9aa6d9f536544e2
Kristof Buts <kristof.buts@gmail.com>
2012-04-04 09:23:46 +0200

    fixed warnings

--- src/test/java/be/butskri/classpathscanning/alternate/FindAnnotatedClassesTest.java
+++ src/test/java/be/butskri/classpathscanning/alternate/FindAnnotatedClassesTest.java
@@ -1,9 +1,9 @@
 package be.butskri.classpathscanning.alternate;
 
-import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
-@RunWith(JUnit4ClassRunner.class)
+@RunWith(BlockJUnit4ClassRunner.class)
 public class FindAnnotatedClassesTest {
 
 }
