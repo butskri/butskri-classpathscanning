350bf407c05b75bdfc6d2c53297e78d8af74748c
Kristof Buts <kristof.buts@gmail.com>
2012-04-19 09:00:41 +0200

    moved test class to test dir + added test method to test class

--- src/test/java/be/butskri/classpathscanning/alternate/FindAnnotatedClassesTest.java
+++ src/test/java/be/butskri/classpathscanning/alternate/FindAnnotatedClassesTest.java
@@ -1,9 +1,15 @@
 package be.butskri.classpathscanning.alternate;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class FindAnnotatedClassesTest {
+	
+	@Test
+	public void testsNothing() {
+		
+	}
 
 }
